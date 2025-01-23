ef1528dd81f3d64fe913c2af791d5f1b2ebe9e66
cristianperez8 <119428833+cristianperez8@users.noreply.github.com>
2025-01-23 22:39:59 +0100

    Base de Datos modificada

--- BaseDeDatos.sql
+++ BaseDeDatos.sql
@@ -2,6 +2,8 @@ CREATE DATABASE IF NOT EXISTS practica3DB;
 
 USE practica3DB;
 
+-- Crear la tabla usuario
+
 CREATE TABLE IF NOT EXISTS usuario (
     alias VARCHAR(50) PRIMARY KEY,
     password VARCHAR(50) NOT NULL,
@@ -15,3 +17,24 @@ INSERT INTO usuario (alias, password, nombre, apellidos, fecha_nacimiento) VALUE
 ('diegoto88', '123456', 'Diego', 'Rodríguez Rebolló', '2002-07-11'),
 ('cr8perez', 'cris', 'Cristian', 'Pérez García', '2003-04-28'),
 ('ngale', '1234', 'Alejandro', 'Castro Domínguez', '1999-01-01');
+
+-- Crear la tabla de amigos
+CREATE TABLE amigos(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_alias VARCHAR(50) NOT NULL,
+    amigo_alias VARCHAR(50) NOT NULL,
+    status ENUM('pendiente', 'aceptado', 'rechazado') DEFAULT 'pendiente',
+    FOREIGN KEY (usuario_alias) REFERENCES usuario(alias),
+    FOREIGN KEY (amigo_alias) REFERENCES usuario(alias)
+);
+
+-- Crear la tabla de mensajes
+CREATE TABLE mensajes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    envia_alias VARCHAR(50) NOT NULL,
+    recibe_alias VARCHAR(50) NOT NULL,
+    mensaje TEXT NOT NULL,
+    hora_envio TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (envia_alias) REFERENCES usuario(alias),
+    FOREIGN KEY (recibe_alias) REFERENCES usuario(alias)
+);
